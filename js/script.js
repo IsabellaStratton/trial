ab01d695aed7cdf9ba14ca2c1426790d051c5ff6
IsabellaStratton <isabellastratton97@gmail.com>
2016-09-20 17:31:33 +1200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var endpoint = 'http://api.opencagedata.com/geocode/v1/json?q=';
 // The full url will be constructed like this
 // http://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=YOUR-API-KEY
 function findLocation(){
-	var query = ("#text");
+	var query = 'Wellington, New Zealand';
 
 	//The query needs to be encoded for URLs - ie replaces spaces with %20's
  	var encodedQuery = encodeURIComponent(query);
